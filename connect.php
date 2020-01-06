305f23f0e05a4aaf8786a42f82f6f2d9ae2c9aa2
Crawcusty <58744047+Crawcusty@users.noreply.github.com>
2020-01-06 18:24:56 +0100

    Łączenie
    łączenie z mysql kod

--- connect.php
+++ connect.php
@@ -5,10 +5,9 @@ $user = "root";
 $password = "";
 $name = "kalendarz";
 	
-	
-	
-	      
-
-	
-	  
+		    $con = mysqli_connect($host, $db_user, $db_password, $db_name);
+	    
+		if ($con->connect_error) {
+	    die("Connection failed: " . $link->connect_error);
+	}
 	?>
