74051b00e6acb011a7bf5f9cc802364d311013d3
Piovbi <58751633+Piovbi@users.noreply.github.com>
2020-01-07 21:14:48 +0100

    Update add.php

--- add.php
+++ add.php
@@ -1,1 +1,23 @@
-
+<?php
+	include("connect.php");
+	
+	$Name = $_POST['name'];
+	$Time= $_POST['time'];
+	$Date = $_POST['date'];
+	$Location= $_POST['location'];
+	$Description = $_POST['description'];
+	
+	$sql = "INSERT INTO wydarzenia (name,time,date,location,description) VALUES ('$Name','$Time','$Date', '$Location', '$Description')";
+	 
+	 if(!mysqli_query($con,$sql))
+	 {
+		 echo 'Nie dodano';
+	 }
+	 else
+	 {
+		 echo 'Dodano';
+	 }
+	 
+	 header("refresh:2; url=index.html");
+	 
+	 ?>
