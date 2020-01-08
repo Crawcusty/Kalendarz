901d68eacd249d89b792af8cb07cab1e2ca62a57
Crawcusty <58744047+Crawcusty@users.noreply.github.com>
2020-01-08 20:59:43 +0100

    dodanie kodu

--- show1.php
+++ show1.php
@@ -1,1 +1,20 @@
 
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+	</head>
+	<body>
+	<center>
+	<table action="wyswietlanie.php">
+		<tr>
+			<th>Tytuł wydarzenia:</th>
+			<th>Godzina:</th>
+			<th>Data:</th>
+			<th>Lokalizacja:</th>
+			<th>Opis:</th>
+			
+		
+
+	
+
+	</body>
