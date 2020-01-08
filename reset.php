7f3571ac296a3f30a7905f70c5c358c50e02b3eb
Crawcusty <58744047+Crawcusty@users.noreply.github.com>
2020-01-08 19:29:52 +0100

    update kodu
    konczenie kodu

--- reset.php
+++ reset.php
@@ -6,6 +6,16 @@ include("connect.php");
 
 
 $sql = "TRUNCATE `kalendarz`.`wydarzenia`";
+ if(!mysqli_query($con,$sql))
+ {
+	 echo 'Nie można zresetowac tabeli';
+ }
+ else
+ {
+	 echo 'Tabela została zresetowana.';
+ }
+ 
+ header("refresh:2; url=index.html");
 
 
 
