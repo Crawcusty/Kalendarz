ba79f921a75caab01d954c6ca00b3c483f9ba16f
Crawcusty <58744047+Crawcusty@users.noreply.github.com>
2020-01-08 20:15:46 +0100

    dodawanie kodu
    dodawanie kodu

--- show.php
+++ show.php
@@ -1,1 +1,12 @@
 
+<?php
+	include("connect.php");
+	$sql = "SELECT * FROM wydarzenia";
+	 
+	$records = mysqli_query($con,$sql);
+	 if(!mysqli_query($con,$sql))
+	 {
+		 echo 'Nie udało się nic wyświetlić.';
+	 }
+	
+	 ?>
